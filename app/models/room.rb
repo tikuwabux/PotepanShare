ef6f2b6fdb750558dfd5013c2a945d8ee3d2f01c
齋藤真一 <saitoshinichi@saitoushinichinoMacBook-Air.local>
2022-11-24 23:22:27 +0900

    Associate User_model and Room_model & Add ActiveStrage in Room_model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  # userモデルに関連付ける(userモデル:roomモデル == 1:多)｡FKはデフォルトのuser_id
+  belongs_to :user
+  # このモデルでAcitve Strageを使用する
+  # 1つのroomに1つの画像を紐付けること､その画像をRoomモデルからimageと呼ぶことを指定
+  has_one_attached :image
 end
