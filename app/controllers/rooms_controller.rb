d6a409ed15cafaee0c043754ffea141f00aaf620
齋藤真一 <saitoshinichi@saitoushinichinoMacBook-Air.local>
2022-11-29 02:46:21 +0900

    Fix CRUD_C of Room_model to correspond to the new relation

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,12 +5,10 @@ class RoomsController < ApplicationController
   end
 
   def create
-    # current_user(gem'device'を導入したモデル名) → deviseのヘルパーメソッド｡現在ログインしているユーザーのレコードを取得｡
-    # .rooms → 紐付いたRoomオブジェクトらの一覧を得る
-    # .new() → その一覧に､()内の値をもつRoomオブジェクトを追加
-    # room_params → 下で定義しているprivateメソッド
-    @room = current_user.rooms.new(room_params)
-
+    # room_params は 下端で定義しているprivateメソッド
+    # Roomオブジェクトのuser_idカラム値は､ログイン中Userオブジェクトのidカラム値と一致させたい｡
+    # だから､Reservationモデルを介した関連を利用しない方法で､後者を前者に渡し､下の記述で受け取っている｡
+    @room = Room.new(room_params)
     # 生成したroomインスタンスをDBへ保存することが成功した時
     if @room.save
       flash[:notice] = "ルームの新規登録に成功しました"
@@ -42,8 +40,8 @@ class RoomsController < ApplicationController
 
   private
   # params を使用し､ルーム情報登録ページの送信ボタン押下､ルーム情報編集ページの編集ボタン押下によって送られたきたデータを取得｡
-  # 尚､require,permitメソッドで､roomオブジェクトのname･introduction・price･address・･image属性以外の取得データを弾く｡(Strong Parameters)
+  # 尚､require,permitメソッドで､roomオブジェクトのname･introduction・price･address・image･user_id属性以外の取得データを弾く｡(Strong Parameters)
   def room_params
-    params.require(:room).permit(:name, :introduction, :price, :address, :image)
+    params.require(:room).permit(:name, :introduction, :price, :address, :image, :user_id)
   end
 end
