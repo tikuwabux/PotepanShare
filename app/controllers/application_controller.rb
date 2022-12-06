f19bbfd97b370426d42fe187874e35ac75b158e6
齋藤真一 <saitoshinichi@saitoushinichinoMacBook-Air.local>
2022-12-07 02:54:05 +0900

    Prepare room_search_form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # 全コントローラーの全アクションに対応するviewファイルで､room検索フォームを作成可能にする
+  before_action :prepare_room_search_form
+
+  # room検索フォームを作成するために必要なroomインスタンスを生成し､格納
+  def prepare_room_search_form
+    @search_results = Room.ransack(params[:q])
+  end
 end
