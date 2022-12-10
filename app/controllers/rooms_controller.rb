4f913c19866290f85d3c4d0637a14c8e494a30ff
齋藤真一 <saitoshinichi@saitoushinichinoMacBook-Air.local>
2022-12-10 09:12:51 +0900

    Get the values_sent_from_search_form by runsack

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -26,7 +26,7 @@ class RoomsController < ApplicationController
   end
 
   def show
-    # Room.find(params[:id]) で､idがparams[:id]であるroomレコードを取得
+    # Room.find(params[:id]) で､idカラム値がparams[:id]であるroomレコードを取得
     @room = Room.find(params[:id])
 
     # ルーム詳細ページに､ルーム予約フォームを入れたいので､その作成に使うReservationインスタンス(値はまだ空)を生成｡
@@ -41,22 +41,55 @@ class RoomsController < ApplicationController
     #rooms = current_user.rooms という関連を使った記述でも可
   end
 
+  # room検索フォーム1 (検索条件 == 住所名 && 単数単語)(シンプルモード)の検索結果表示ページに対応
   def address_search
-    @q = Room.ransack(params[:q])
-    @results = @q.result
+
+    @search = Room.ransack(params[:q])
+    # (room検索フォーム1作成時に､ params[:q] = {:address_cont => 'フォームの入力値'} と設計したので)
+    #       = Room.ransack({:address_cont => 'フォームの入力値'})
+    #       => Ransack::Search<class: Room, base: Grouping <conditions: [Condition <attributes: ["address"], predicate: cont, values: ["フォームの入力値"]>], combinator: and>>
+
+    # .resultで､ransackメソッドで取得したデータをActiveRecord_Relationのオブジェクトに変換｡
+    @search_rooms = @search.result
+
+    # オブジェクトを取得するまでに､どんなPostgreSQLクエリを発行したのか､以下で確認
+    # @search_rooms.sql => SELECT "rooms".* FROM "rooms" WHERE "rooms"."address" ILIKE '%東京%'
+    # roomsテーブルから､addressカラム値が '%フォームの入力値%'である部分一致のILIKE検索(大文字小文字を個別しないLIKE検索)を実行し､該当レコードを取得という内容
   end
 
-  # def freeword_name_search
-  #   binding.pry
-  #   if params[:q] != nil
-  #     params[:q]['name_cont_any'] = params[:q]['name_cont_any'].split(/[\p{blank}\s]+/)
-  #     @keyword = Room.ransack(params[:q])
-  #     @rooms = @keyword.result
-  #   else
-  #     @keyword = Room.ransack(params[:q])
-  #     @rooms = @keyword.result #検索の結果を受け取る。
-  #   end
-  # end
+  # room検索フォーム2 (検索条件 == ルーム名 && (単数単語 || 複数単語) )(アドバンストモード)の検索結果表示ページに対応
+  def freeword_name_search
+    # 具体例として､ "お  菓子 家" とフォームに入力後､送信ボタン押下時､
+    # つまり､params[:q][:name_cont] == "お  菓子 家" の時､に実行される処理を 1→2→3→4→5 で示す
+
+    #以下2行で groupings属性に渡す値を設計する｡
+
+    # .split(全角･半角スペースの正規表現)で､入力値(文字列)を半角・全角スペース区切りで配列に変換
+    keywords = params[:q][:name_cont].split(/[\p{blank}\s]+/)
+    #1       = "お  菓子 家".split(/[\p{blank}\s]+/)
+    #        => ["お", "菓子", "家"]
+
+    # reduceを使って､keywords(文字列(複数可)を要素にもつ配列)を､grouping_array(条件(複数可､ハッシュ構造)をもつ配列)に変換する｡
+    grouping_array = keywords.reduce([]){ |array, word| array << { name_cont: word } }
+    #2             = ["お", "菓子", "家"].reduce([]){ |array, word| array << { name_cont: word } }
+    #              => [{:name_cont=>"お"}, {:name_cont=>"菓子"}, {:name_cont=>"家"}]
+
+    # 設計したgrouping_arrayの条件でAND検索を実行
+    @search = Room.ransack({ combinator: 'and', groupings: grouping_array })
+    #3      = Room.ransack({ combinator: 'and', groupings: [{:name_cont=>"お"}, {:name_cont=>"菓子"}, {:name_cont=>"家"}] })
+    #       => Ransack::Search<class: Room, base: Grouping <combinator: and, groupings: [
+    #                                                                                    Grouping <conditions: [Condition <attributes: ["name"], predicate: cont, values: ["お"]>]>,
+    #                                                                                    Grouping <conditions: [Condition <attributes: ["name"], predicate: cont, values: ["菓子"]>]>,
+    #                                                                                    Grouping <conditions: [Condition <attributes: ["name"], predicate: cont, values: ["家"]>]>
+    #                                                                                    ] >>
+
+    @search_rooms = @search.result
+    #4 => [#<Room:0x00000001629ee450 id: 8, name: "お菓子の家", introduction: "お菓子の家です", price: 1000, address: "東京大田区東武練馬", user_id: 4, created_at: Sat, 26 Nov 2022 22:07:02 UTC +00:00, updated_at: Sat, 26 Nov 2022 22:07:02 UTC +00:00>]
+
+    #5 オブジェクトを取得するまでに､どんなPostgreSQLクエリを発行したのか､以下で確認
+    # @search_rooms.sql => SELECT "rooms".* FROM "rooms" WHERE ("rooms"."name" ILIKE '%お%' AND "rooms"."name" ILIKE '%菓子%' AND "rooms"."name" ILIKE '%家%')
+    # roomsテーブルから､nameカラム値が '%お%' かつ '%菓子%' かつ %家% である部分一致のILIKE検索を実行し､該当レコードを取得という内容｡
+  end
 
   private
   # params を使用し､ルーム情報登録ページの送信ボタン押下､ルーム情報編集ページの編集ボタン押下によって送られたきたデータを取得｡
