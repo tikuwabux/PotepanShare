38b7844cb7a56c414fbc6ad938b6b803a82b0b21
齋藤真一 <saitoshinichi@saitoushinichinoMacBook-Air.local>
2022-12-10 09:19:04 +0900

    Fix spell

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
   # 全コントローラーの全アクションに対応するviewファイルで､room検索フォームを作成可能にする
   before_action :prepare_room_search_form
 
-  # room検索フォームを作成するために必要なroomインスタンスを生成し､格納
+  # room検索フォームを作成するために必要な(search_form_forの引数となる)searchオブジェクトを生成し､格納
   def prepare_room_search_form
-    @search_results = Room.ransack(params[:q])
+    @search = Room.ransack(params[:q])
+
+    # @searchオブジェクトのクラスを確認
+    # @search.class => Ransack::Search
   end
 end
